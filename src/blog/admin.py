f64eacf9c1e8950cd86b935478f43c096fc3a98d
malika65 <malika.satimbaeva@iaau.edu.kg>
2021-02-02 21:37:54 +0600

    Templates added

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -3,6 +3,7 @@ from django.contrib.admin import ModelAdmin
 from trumbowyg.widgets import TrumbowygWidget
 from .models import Post, Category
 from django.forms import ModelForm
+from modeltranslation.admin import TranslationAdmin
 
 
 class PostAdminForm(ModelForm):
@@ -16,6 +17,21 @@ class PostAdminForm(ModelForm):
 class PostAdmin(admin.ModelAdmin):
     form = PostAdminForm
 
+
+
+
+# @admin.register(Post)
+# class PostModelAdmin(TranslationAdmin):
+#     list_display = ("title", "body")
+
+
+# @admin.register(Category)
+# class CategoryAdmin(TranslationAdmin):
+#     list_display = ("title")
+    
+
 admin.site.register(Category)
 
 admin.site.register(Post, PostAdmin)
+
+
--- src/main/urls.py
+++ src/main/urls.py
@@ -17,11 +17,23 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('trumbowyg/', include('trumbowyg.urls')),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
+
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += i18n_patterns(
+    path('blog/', include('blog.urls')),
+
+    
+)
