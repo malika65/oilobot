8bd3a11989243637d84aaf9916592281a16a4798
malika65 <malika.satimbaeva@iaau.edu.kg>
2021-02-02 18:51:22 +0600

    Add database postgres

--- src/main/settings.py
+++ src/main/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'oilobot',
+        'USER': 'maks',
+        'PASSWORD': 'maks123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
 }
 
 
