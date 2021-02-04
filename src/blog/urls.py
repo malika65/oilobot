19a3c098b612e6426bd6ce1d61bab8fe607a0403
Kumarbek-sydygaliev <kumarbekdysygaliev44@gmail.com>
2021-02-04 11:21:56 +0600

    questions and answers view

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+
+from .views import questions_view_ru, answer_view_ru, questions_view_kg, answer_view_kg
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    
-
+    path('questions-ru', questions_view_ru),
+    path('answer-ru/<int:answer_id>', answer_view_ru),
+    path('questions-kg', questions_view_kg),
+    path('answer-kg/<int:answer_id>', answer_view_kg),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
+
+def questions_view_ru(request):
+    dummy = Question.objects.all()
+    questions = []
+    for i in dummy:
+        questions.append({'id':i.id, 'question':i.question_ru, 'answer':i.answer_ru})
+    print(questions)
+    return render(request, 'questions.html', {'questions':questions, 'lang':'ru'})
+
+def answer_view_ru(request, answer_id):
+    print(answer_id)
+    answer = Question.objects.get(id=answer_id)
+    print(1)
+    return render(request, 'answer.html', {'answer':answer.answer_ru, 'lang':'ru'})
+
+def questions_view_kg(request):
+    dummy = Question.objects.all()
+    questions = []
+    for i in dummy:
+        questions.append({'id':i.id, 'question':i.question_kg, 'answer':i.answer_kg})
+    print(questions)
+    return render(request, 'questions.html', {'questions':questions, 'lang':'kg'})
+
+def answer_view_kg(request, answer_id):
+    print(answer_id)
+    answer = Question.objects.get(id=answer_id)
+    print(1)
+    return render(request, 'answer.html', {'answer':answer.answer_kg, 'lang':'kg'})
